bc763e8ae45e75519281c849c093828961b47598
Alukard <67117472+AniMate002@users.noreply.github.com>
2024-10-17 00:39:13 +0200

    Small fixes 0.0.8.3 Small fixes

--- app/Fonts.ts
+++ app/Fonts.ts
@@ -2,15 +2,19 @@ import { Space_Grotesk, Montserrat, Nunito_Sans} from 'next/font/google'
 
 export const space_font = Space_Grotesk({
   subsets: ['latin'],
-  display: 'fallback'
+  display: 'fallback',
 })
 
 export const montserrat_font = Montserrat({
   subsets: ['latin'],
-  display: 'fallback'
+  display: 'fallback',
+  adjustFontFallback: false
+
 })
 
 export const nunito_font = Nunito_Sans({
   subsets: ['latin'],
-  display: 'swap'
+  display: 'swap',
+  adjustFontFallback: false
+
 })
