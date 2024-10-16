b2ffd79177ec0d38254dffaa0bc38a1e3a048cf8
Alukard <67117472+AniMate002@users.noreply.github.com>
2024-10-17 01:52:07 +0200

    v0.0.10 Login page added

--- app/app/signup/page.tsx
+++ app/app/signup/page.tsx
@@ -5,23 +5,24 @@ import Link from 'next/link';
 
 
 
-const Login:React.FC = () => {
+const Signup:React.FC = () => {
     return (
         <>
             <div className="relative h-[100vh] w-[100vw] flex items-center justify-center flex-col text-white">
                 <img src={bg.src} className="absolute z-[-1] top-0 left-0 w-[100vw] h-[100vh]"/>
                 <h2 className={`${montserrat_font.className} text-6xl tracking-widest`}>Lueur</h2>
-                <p className='mt-32 text-lg'>Have an account?</p>
+                <p className='mt-32 text-lg'>Don&apos;t have an account?</p>
                 <form className='mt-10 flex items-center flex-col gap-4 w-[400px]'>
+                    <input type="name" placeholder='Username' className='bg-[#31473a] px-8 py-4 text-lg rounded-full w-full'/>
                     <input type="email" placeholder='Email' className='bg-[#31473a] px-8 py-4 text-lg rounded-full w-full'/>
                     <input type="password" placeholder='Password' className='bg-[#31473a] px-8 py-4 text-lg rounded-full w-full'/>
-                    <button className='text-[#658874] border-2 border-[#31473a] w-full block py-4 font-bold rounded-xl mt-10 hover:backdrop-blur-sm transition-all duration-200'>Log in</button>
+                    <button className='text-[#658874] border-2 border-[#31473a] w-full block py-4 font-bold rounded-xl mt-10 hover:backdrop-blur-sm transition-all duration-200'>Sign up</button>
                     <button className='hover:underline'>Forgot password?</button>
-                    <Link href={'/signup'} className='text-[#658874]'>or Sign up</Link>
+                    <Link href={'/login'} className='text-[#658874]'>or Log in</Link>
                 </form>
             </div>
         </>
     )
 }
 
-export default Login
+export default Signup
