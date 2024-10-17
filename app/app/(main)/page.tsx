bec993819f5f7aee6170d30584af21aaa3951023
Alukard <67117472+AniMate002@users.noreply.github.com>
2024-10-17 22:01:34 +0200

    v0.1.0.2 Small fixes

--- app/app/(main)/page.tsx
+++ app/app/(main)/page.tsx
@@ -4,14 +4,14 @@ import Stories from "@/components/Stories/Stories";
 import { IPost } from "@/lib/Models/Post.model";
 import axios from "axios";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 
 export default function Home() {
   const [posts, setPosts] = useState<Array<IPost>>([]);
   const { status } = useSession()
-  const router = useRouter()
+  // const router = useRouter()
   async function fetchPosts() {
     const res = await axios.get('/api/posts')
     return res.data
